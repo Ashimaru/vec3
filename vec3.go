12bd4312b9a5aef59bee4a534b1c5ed5eac26087
Mateusz Michalak <ucho26@gmail.com>
2023-04-30 20:58:31 +0000

    Add more basic operations

--- vec3.go
+++ vec3.go
@@ -4,8 +4,17 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
+const EPS = 0.000001
+
 type Flt interface {
 	constraints.Float
+}
+
+func abs[Num Flt](val Num) Num {
+	if val < 0 {
+		return -val
+	}
+	return val
 }
 
 type Vec3[Num Flt] struct {
@@ -22,4 +31,34 @@ func Cross[Num Flt](self *Vec3[Num], other *Vec3[Num]) Vec3[Num] {
 		Y: self.Z*other.X - other.X*other.Z,
 		Z: self.X*other.Y - other.Y*other.X,
 	}
+}
+
+func Add[Num Flt](self *Vec3[Num], other *Vec3[Num]) Vec3[Num] {
+	return Vec3[Num]{
+		X: self.X + other.X,
+		Y: self.Y + other.Y,
+		Z: self.Z + other.Z,
+	}
+}
+
+func Sub[Num Flt](self *Vec3[Num], other *Vec3[Num]) Vec3[Num] {
+	return Vec3[Num]{
+		X: self.X - other.X,
+		Y: self.Y - other.Y,
+		Z: self.Z - other.Z,
+	}
+}
+
+func MulScal[Num Flt](self *Vec3[Num], val Num) Vec3[Num] {
+	return Vec3[Num]{
+		X: self.X * val,
+		Y: self.Y * val,
+		Z: self.Z * val,
+	}
+}
+
+func Equals[Num Flt](self *Vec3[Num], other *Vec3[Num]) bool {
+	return abs((self.X-other.X)) < EPS &&
+		abs((self.Y-other.Y)) < EPS &&
+		abs((self.Z-other.Z)) < EPS
 }
