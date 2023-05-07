5c2d40a0ca5094c51ab14fe5ea068ffe7feafc8f
ashimaru <ucho26@gmail.com>
2023-05-07 14:46:11 +0200

    Add basic operations vector

--- vec3.go
+++ vec3.go
@@ -1,6 +1,8 @@
 package vec3
 
 import (
+	"math"
+
 	"golang.org/x/exp/constraints"
 )
 
@@ -21,31 +23,33 @@ type Vec3[Num Flt] struct {
 	X, Y, Z Num
 }
 
-func Dot[Num Flt](self *Vec3[Num], other *Vec3[Num]) Num {
+type elementOp[Num Flt] func(Num, Num) Num
+
+func piecewiseOp[Num Flt](v1 Vec3[Num], v2 Vec3[Num], op elementOp[Num]) Vec3[Num] {
+	return Vec3[Num]{
+		op(v1.X, v2.X),
+		op(v1.Y, v2.Y),
+		op(v1.Z, v2.Z),
+	}
+}
+
+func constwiseOp[Num Flt](v1 Vec3[Num], val Num, op elementOp[Num]) Vec3[Num] {
+	return Vec3[Num]{
+		op(v1.X, val),
+		op(v1.Y, val),
+		op(v1.Z, val),
+	}
+}
+
+func Dot[Num Flt](self Vec3[Num], other Vec3[Num]) Num {
 	return self.X*other.X + self.Y*other.Y + self.Z*other.Z
 }
 
-func Cross[Num Flt](self *Vec3[Num], other *Vec3[Num]) Vec3[Num] {
+func Cross[Num Flt](self Vec3[Num], other Vec3[Num]) Vec3[Num] {
 	return Vec3[Num]{
 		X: self.Y*other.Z - other.Z*other.Y,
 		Y: self.Z*other.X - other.X*other.Z,
 		Z: self.X*other.Y - other.Y*other.X,
-	}
-}
-
-func Add[Num Flt](self *Vec3[Num], other *Vec3[Num]) Vec3[Num] {
-	return Vec3[Num]{
-		X: self.X + other.X,
-		Y: self.Y + other.Y,
-		Z: self.Z + other.Z,
-	}
-}
-
-func Sub[Num Flt](self *Vec3[Num], other *Vec3[Num]) Vec3[Num] {
-	return Vec3[Num]{
-		X: self.X - other.X,
-		Y: self.Y - other.Y,
-		Z: self.Z - other.Z,
 	}
 }
 
@@ -57,8 +61,36 @@ func MulScal[Num Flt](self *Vec3[Num], val Num) Vec3[Num] {
 	}
 }
 
-func Equals[Num Flt](self *Vec3[Num], other *Vec3[Num]) bool {
-	return abs((self.X-other.X)) < EPS &&
-		abs((self.Y-other.Y)) < EPS &&
-		abs((self.Z-other.Z)) < EPS
+func Add[Num Flt](self Vec3[Num], other Vec3[Num]) Vec3[Num] {
+	return piecewiseOp(self, other, func(a, b Num) Num { return a + b })
+}
+
+func Sub[Num Flt](self Vec3[Num], other Vec3[Num]) Vec3[Num] {
+	return piecewiseOp(self, other, func(a, b Num) Num { return a - b })
+}
+
+func Length[Num Flt](self Vec3[Num]) Num {
+	squaredSum := self.X * self.X
+	squaredSum += self.Y * self.Y
+	squaredSum += self.Z * self.Z
+	return Num(math.Sqrt(float64(squaredSum)))
+}
+
+func Normalize[Num Flt](self Vec3[Num]) Vec3[Num] {
+	len := Length(self)
+	return Vec3[Num]{self.X / len, self.Y / len, self.Z / len}
+}
+
+func MulScalar[Num Flt](self Vec3[Num], val Num) Vec3[Num] {
+	return constwiseOp(self, val, func(a, b Num) Num { return a * b })
+}
+
+func DivScalar[Num Flt](self Vec3[Num], val Num) Vec3[Num] {
+	return constwiseOp(self, val, func(a, b Num) Num { return a / b })
+}
+
+func Equals[Num Flt](self Vec3[Num], other Vec3[Num]) bool {
+	return abs(self.X-other.X) < EPS &&
+		abs(self.Y-other.Y) < EPS &&
+		abs(self.Z-other.Z) < EPS
 }
---
+++ vec3_test.go
@@ -0,0 +1,104 @@
+package vec3
+
+import (
+	"math"
+	"testing"
+)
+
+type TestVec = Vec3[float32]
+
+func TestAdd(t *testing.T) {
+	a := TestVec{1, 2, 3}
+	b := TestVec{8, 2, -4}
+
+	result := Add(a, b)
+
+	expected := TestVec{9, 4, -1}
+	if !Equals(result, expected) {
+		t.Errorf("Got %v vs expected %v", result, expected)
+	}
+}
+
+func TestMul(t *testing.T) {
+	a := TestVec{1, 2, 3}
+	b := TestVec{8, 2, -4}
+
+	result := Sub(a, b)
+
+	expected := TestVec{-7, 0, 7}
+	if !Equals(result, expected) {
+		t.Errorf("Got %v vs expected %v", result, expected)
+	}
+}
+
+func TestCross(t *testing.T) {
+	a := TestVec{0, 0, 1}
+	b := TestVec{1, 0, 0}
+
+	result := Cross(a, b)
+	expected := TestVec{0, 1, 0}
+
+	if !Equals(result, expected) {
+		t.Errorf("Got %v vs expected %v", result, expected)
+	}
+}
+
+func TestDot(t *testing.T) {
+	a := TestVec{1, 2, 3}
+	b := TestVec{4, 5, 6}
+
+	result := Dot(a, b)
+	if result != 32 {
+		t.Errorf("Got %v vs expected %v", result, 32)
+	}
+}
+
+func TestLength(t *testing.T) {
+	test := func(tested TestVec, expectedResult float32) {
+		len := Length(tested)
+		if len != expectedResult {
+			t.Errorf("Got %f, expected %f", len, expectedResult)
+		}
+	}
+
+	test(TestVec{1, 0, 0}, 1)
+	test(TestVec{1, 1, 1}, float32(math.Sqrt(3)))
+}
+
+func TestNormalize(t *testing.T) {
+	test := func(tested TestVec, expectedResult TestVec) {
+		len := Normalize(tested)
+		if len != expectedResult {
+			t.Errorf("Got %v, expected %v", len, expectedResult)
+		}
+	}
+
+	test(TestVec{1, 0, 0}, TestVec{1, 0, 0})
+	test(TestVec{1, 1, 1}, TestVec{float32(1 / math.Sqrt(3)), float32(1 / math.Sqrt(3)), float32(1 / math.Sqrt(3))})
+}
+
+func TestMulScalar(t *testing.T) {
+	test := func(tested TestVec, val float32, expectedResult TestVec) {
+		result := MulScalar(tested, val)
+		if result != expectedResult {
+			t.Errorf("Got %v, expected %v", result, expectedResult)
+		}
+	}
+
+	test(TestVec{1, 0, 0}, 3, TestVec{3, 0, 0})
+	test(TestVec{2, 0, 0}, 3, TestVec{6, 0, 0})
+	test(TestVec{1, 1, 1}, 3, TestVec{3, 3, 3})
+}
+
+func TestDivScalar(t *testing.T) {
+	test := func(tested TestVec, val float32, expectedResult TestVec) {
+		result := DivScalar(tested, val)
+		if result != expectedResult {
+			t.Errorf("Got %v, expected %v", result, expectedResult)
+		}
+	}
+
+	test(TestVec{1, 0, 0}, 3, TestVec{1.0 / 3.0, 0, 0})
+	test(TestVec{2, 0, 0}, 3, TestVec{2.0 / 3.0, 0, 0})
+	test(TestVec{1, 1, 1}, 3, TestVec{1.0 / 3.0, 1.0 / 3.0, 1.0 / 3.0})
+}
